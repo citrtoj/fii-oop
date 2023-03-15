5439e421fd113405c5750ca5e611ff322e11b6b3
Sabina <citrtoj@gmail.com>
2023-03-15 20:40:04 +0200

    Finished Lab 1

--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <conio.h>
 
+//ex 2
 int myAtoi(const char* text) {
 	int s = 0;
 	int i = 0;
@@ -17,6 +18,7 @@ int myAtoi(const char* text) {
 
 
 int main() {
+	//ex 2
 	char buffer[256];
 	FILE* f = fopen("in.txt", "r");
 	int sum = 0;
@@ -24,26 +26,69 @@ int main() {
 		sum += myAtoi(buffer);
 		strcpy(buffer, "");
 	}
-	printf("%d", sum);
+	printf("%d\n", sum);
 
 	//ex 3
-	char words[128][256] = { 0 };
-	if (scanf("%s", buffer)) {
+	char* words[256] = {0}; //vector de pointeri la cuvinte stocate pe heap
+	int n = 0;
+	if (scanf("%[^\n]s", buffer)) {
 		char* p;
-		int n = 0;
 		p = strtok(buffer, " ");
 		while (p) {
-			if (n == 0) {
-				strcpy(words[0], p);
+			if (n <= 0) {
+				if (words[0] = new char[strlen(p)]) {
+					strcpy(words[0], p);
+				}
 			}
 			else {
-				//insertion sort
+				int j = n - 1;
+				char* element = new char[strlen(p)];
+				strcpy(element, p);
+				while (j >= 0 && ((strlen(words[j]) < strlen(p)) || ( (strlen(words[j]) == strlen(p) && strcmp(words[j], p) > 0)))) {
+					words[j + 1] = words[j];
+					--j;
+				}
+				words[j + 1] = element;
 			}
+			
+			++n;
+			p = strtok(NULL, " ");
 		}
 	}
 	else {
 		printf("error reading sentence from input!");
 	}
+	for (int i = 0; i < n; ++i) {
+		std::cout << words[i] << " ";
+	}
 
+	printf("\nPress any key to exit...");
+	std::cin >> n;
 	return 0;
 }
+
+/*
+* ex 4
+#include <iostream>
+using namespace std;
+
+bool isPrime(int n)
+{
+	for (int tr = 2; tr < n / tr; tr++)
+		if ((n % tr ) == 0)
+			return false;
+	return true;
+}
+
+int main()
+{
+	int n;
+	std::cout << "Enter a number:";
+	std::cin >> n;
+	if (isPrime(n))
+		std::cout << n << " is prime !";
+	else
+		std::cout << n << " is NOT prime !";
+	return 0;
+}
+*/
